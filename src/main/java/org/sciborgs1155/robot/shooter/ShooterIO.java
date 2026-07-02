116563c4f6d4850431701d47589f36cb9099d7d5
Tirtha <tirthas970@gmail.com>
2026-07-02 12:58:33 -0400

    fixed the build issue again

--- src/main/java/org/sciborgs1155/robot/shooter/ShooterIO.java
+++ src/main/java/org/sciborgs1155/robot/shooter/ShooterIO.java
@@ -2,31 +2,29 @@ package org.sciborgs1155.robot.shooter;
 
 public interface ShooterIO {
 
-    /*
-     * sets voltage of flywheel
-     * @param voltage The voltage
-     */
+  /*
+   * sets voltage of flywheel
+   * @param voltage The voltage
+   */
 
-    void setFlyWheelVoltage(double voltage);
+  void setFlyWheelVoltage(double voltage);
 
-    /*
-     * sets voltage of variable hood
-     * @param voltage The voltage
-     */
-    void setHoodVoltage(double voltage);
+  /*
+   * sets voltage of variable hood
+   * @param voltage The voltage
+   */
+  void setHoodVoltage(double voltage);
 
-    /*
-     * velocity of the flywheel (in radians per sec)
-     * @return The velocity of the flywheel, in radians per sec
-     *
-     */
-    double flyWheelVelocity();
+  /*
+   * velocity of the flywheel (in radians per sec)
+   * @return The velocity of the flywheel, in radians per sec
+   *
+   */
+  double flyWheelVelocity();
 
-    /*
-     * Posotion of hood in radian 
-     * @returns the position of hood in radians
-     */
-    double getHoodPosition();
-
-
+  /*
+   * Posotion of hood in radian
+   * @returns the position of hood in radians
+   */
+  double getHoodPosition();
 }
