88d1f8840cf94069039f6418a7bc704191c6b4fe
Tirtha <tirthas970@gmail.com>
2026-07-03 17:56:33 -0400

    working on sim, flywheel stuff is done; need to work on hood sim motor

--- src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
+++ src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
@@ -8,4 +8,31 @@ public class ShooterConstants {
   public static final double GEAR_RATIO = 10.0;
   public static final Current STATOR_CURRENT_LIMIT = Amps.of(30);
   public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(30);
+
+  /* Below are control constants for flyhweel and hood
+   * I'm using the Rebuilt values for now
+   * Will change later
+   */
+
+  /* For Flywheel */
+  public static class VelocityControl {
+    public static final double P = 0.03;
+    public static final double I = 0.0;
+    public static final double D = 0.0;
+
+    public static final double S = 0.0;
+    public static final double V = 0.016981;
+    public static final double A = 0.0021296;
+  }
+
+  /* For hood */
+  public static class AngularControl {
+    public static final double P = 0.03;
+    public static final double I = 0.0;
+    public static final double D = 0.0;
+
+    public static final double S = 0.0;
+    public static final double V = 0.016981;
+    public static final double A = 0.0021296;
+  }
 }
--- src/main/java/org/sciborgs1155/robot/shooter/SimShooter.java
+++ src/main/java/org/sciborgs1155/robot/shooter/SimShooter.java
@@ -1,3 +1,34 @@
 package org.sciborgs1155.robot.shooter;
 
-public class SimShooter {}
+import static edu.wpi.first.units.Units.Seconds;
+import static org.sciborgs1155.robot.Constants.PERIOD;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.AngularControl.*;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.VelocityControl.*;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import org.sciborgs1155.robot.shooter.ShooterConstants.VelocityControl;
+
+public class SimShooter implements ShooterIO {
+  private final FlywheelSim flywheel;
+
+  public SimShooter() {
+    flywheel =
+        new FlywheelSim(
+            LinearSystemId.identifyVelocitySystem(
+                VelocityControl.V, VelocityControl.A), // physical behavior
+            DCMotor.getKrakenX60(1)); // eletrical behavior
+  }
+
+  @Override
+  public void setFlyWheelVoltage(double voltage) {
+    flywheel.setInputVoltage(voltage); // pases the voltage into the flywheel
+    flywheel.update(PERIOD.in(Seconds)); // updates the voltage for 0.02 seconds
+  }
+
+  @Override
+  public double getFlyWheelVelocity() {
+    return flywheel.getAngularVelocityRadPerSec();
+  }
+}
