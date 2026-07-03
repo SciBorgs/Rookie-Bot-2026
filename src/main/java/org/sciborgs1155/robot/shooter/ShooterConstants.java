19690aab76f4f439d2acd229d4a524b5cc02ad01
Tirtha <tirthas970@gmail.com>
2026-07-03 16:22:36 -0400

    FINALLLY!!!!

--- src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
+++ src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
@@ -1,3 +1,14 @@
 package org.sciborgs1155.robot.shooter;
 
-public class ShooterConstants {}
+import edu.wpi.first.units.measure.Current;
+import static edu.wpi.first.units.Units.*;
+
+public class ShooterConstants {
+    public static final double GEAR_RATIO = 10.0;
+    public static final Current STATOR_CURRENT_LIMIT = Amps.of(30);
+    public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(30);
+
+
+
+
+}
