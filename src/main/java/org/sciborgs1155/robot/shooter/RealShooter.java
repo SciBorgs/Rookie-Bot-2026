ef7ac4465562129afa76643c95411e3546552a2c
Tirtha <tirthas970@gmail.com>
2026-07-03 16:27:58 -0400

    spotless

--- src/main/java/org/sciborgs1155/robot/shooter/RealShooter.java
+++ src/main/java/org/sciborgs1155/robot/shooter/RealShooter.java
@@ -1,15 +1,14 @@
 package org.sciborgs1155.robot.shooter;
 
-import static edu.wpi.first.units.Units.Amps;
-import static org.sciborgs1155.robot.shooter.ShooterConstants.*;
-
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import org.sciborgs1155.lib.FaultLogger;
-import org.sciborgs1155.lib.TalonUtils;
 
+/* TO DO:
+ * Fix motor configurations
+ * work on getHoodPosition
+ */
 public class RealShooter implements ShooterIO {
 
   private final TalonFX wheelMotor;
@@ -21,24 +20,13 @@ public class RealShooter implements ShooterIO {
     wheelMotor = new TalonFX(1);
     hoodMotor = new TalonFX(0);
 
-    TalonFXConfiguration configs = new TalonFXConfiguration();
+    MotorOutputConfigs configs = new MotorOutputConfigs();
 
-    configs.MotorOutput.NeutralMode = NeutralModeValue.Brake; // stops the motor
-    configs.MotorOutput.Inverted =
-        InvertedValue.CounterClockwise_Positive; // counterclockwise is postive
-    configs.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT_LIMIT.in(Amps);
-    configs.CurrentLimits.SupplyCurrentLimit = SUPPLY_CURRENT_LIMIT.in(Amps);
+    configs.NeutralMode = NeutralModeValue.Brake;
+    configs.Inverted = InvertedValue.CounterClockwise_Positive;
 
     wheelMotor.getConfigurator().apply(configs);
     hoodMotor.getConfigurator().apply(configs);
-
-    /* Checks the motors */
-    FaultLogger.register(wheelMotor);
-    FaultLogger.register(hoodMotor);
-
-    /* adds motors to a list of all global motors */
-    TalonUtils.addMotor(wheelMotor);
-    TalonUtils.addMotor(hoodMotor);
   }
 
   @Override
@@ -60,9 +48,7 @@ public class RealShooter implements ShooterIO {
   public double getHoodPosition() {
     var currentSig = hoodMotor.getPosition();
     currentSig.refresh();
-    var convertedSig =
-        currentSig.getValueAsDouble()
-            / GEAR_RATIO; // gear ratio to covert motor rotations to physical rotations
+    var convertedSig = currentSig.getValueAsDouble();
     return convertedSig * (2 * Math.PI);
   }
 }
