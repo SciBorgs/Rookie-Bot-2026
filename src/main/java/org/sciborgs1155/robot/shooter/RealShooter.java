44ee306df1092f80ec40c19e8990f02eb7c490ec
Tirtha <tirthas970@gmail.com>
2026-07-03 16:26:05 -0400

    spotless

--- src/main/java/org/sciborgs1155/robot/shooter/RealShooter.java
+++ src/main/java/org/sciborgs1155/robot/shooter/RealShooter.java
@@ -1,9 +1,7 @@
 package org.sciborgs1155.robot.shooter;
 
-
 import static edu.wpi.first.units.Units.Amps;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.*;
-
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -12,24 +10,18 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import org.sciborgs1155.lib.FaultLogger;
 import org.sciborgs1155.lib.TalonUtils;
 
-
 public class RealShooter implements ShooterIO {
-
 
   private final TalonFX wheelMotor;
   private final TalonFX hoodMotor;
 
-
   /* Sets motor configurations */
   public RealShooter() {
-
 
     wheelMotor = new TalonFX(1);
     hoodMotor = new TalonFX(0);
 
-
     TalonFXConfiguration configs = new TalonFXConfiguration();
-
 
     configs.MotorOutput.NeutralMode = NeutralModeValue.Brake; // stops the motor
     configs.MotorOutput.Inverted =
@@ -37,46 +29,40 @@ public class RealShooter implements ShooterIO {
     configs.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT_LIMIT.in(Amps);
     configs.CurrentLimits.SupplyCurrentLimit = SUPPLY_CURRENT_LIMIT.in(Amps);
 
-
     wheelMotor.getConfigurator().apply(configs);
     hoodMotor.getConfigurator().apply(configs);
-
 
     /* Checks the motors */
     FaultLogger.register(wheelMotor);
     FaultLogger.register(hoodMotor);
-
 
     /* adds motors to a list of all global motors */
     TalonUtils.addMotor(wheelMotor);
     TalonUtils.addMotor(hoodMotor);
   }
 
-
   @Override
   public void setFlyWheelVoltage(double voltage) {
     wheelMotor.setVoltage(voltage);
   }
-
 
   @Override
   public void setHoodVoltage(double voltage) {
     hoodMotor.setVoltage(voltage);
   }
 
-
   @Override
   public double getFlyWheelVelocity() {
     return wheelMotor.getVelocity().getValueAsDouble();
   }
-
 
   @Override
   public double getHoodPosition() {
     var currentSig = hoodMotor.getPosition();
     currentSig.refresh();
     var convertedSig =
-        currentSig.getValueAsDouble() / GEAR_RATIO; // gear ratio to covert motor rotations to physical rotations
+        currentSig.getValueAsDouble()
+            / GEAR_RATIO; // gear ratio to covert motor rotations to physical rotations
     return convertedSig * (2 * Math.PI);
   }
 }
--- src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
+++ src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
@@ -1,14 +1,11 @@
 package org.sciborgs1155.robot.shooter;
 
-import edu.wpi.first.units.measure.Current;
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.units.measure.Current;
+
 public class ShooterConstants {
-    public static final double GEAR_RATIO = 10.0;
-    public static final Current STATOR_CURRENT_LIMIT = Amps.of(30);
-    public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(30);
-
-
-
-
+  public static final double GEAR_RATIO = 10.0;
+  public static final Current STATOR_CURRENT_LIMIT = Amps.of(30);
+  public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(30);
 }
