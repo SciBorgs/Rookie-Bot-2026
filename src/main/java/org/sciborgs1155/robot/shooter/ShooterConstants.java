c25b135dca9f8e144ae0faf68a7b72fbcd1e94f8
Tirtha <tirthas970@gmail.com>
2026-07-04 10:46:55 -0400

    working on hood sim, got all the constants done, very close to being done

--- src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
+++ src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
@@ -3,16 +3,27 @@ package org.sciborgs1155.robot.shooter;
 import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+
 
 public class ShooterConstants {
-  public static final double GEAR_RATIO = 10.0;
+
+  /*
+   *ALL OF THESE VALUES ARE PLACE HOLDERS
+   *WILL CHANGE AFTER CAD AND TESTING IS DONE 
+   */
+
+  public static final double GEAR_RATIO = 10.0; //10: 1 ratio (every 10 motor spins = 1 mechanism spin)
   public static final Current STATOR_CURRENT_LIMIT = Amps.of(30);
   public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(30);
+  public static final double MOI = 0.01; //Moment of Intertia 
 
-  /* Below are control constants for flyhweel and hood
-   * I'm using the Rebuilt values for now
-   * Will change later
-   */
+  //copied from rebuilt
+  public static final Distance HOOD_RADIUS = Inches.of(9.29);
+  public static final Angle MIN_ANGLE = Degrees.of(15);
+  public static final Angle MAX_ANGLE = Degrees.of(15);
+  public static final Angle STARING_ANGLE = MIN_ANGLE;
 
   /* For Flywheel */
   public static class VelocityControl {
--- src/main/java/org/sciborgs1155/robot/shooter/SimShooter.java
+++ src/main/java/org/sciborgs1155/robot/shooter/SimShooter.java
@@ -1,34 +1,60 @@
 package org.sciborgs1155.robot.shooter;
 
+import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.Seconds;
 import static org.sciborgs1155.robot.Constants.PERIOD;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.GEAR_RATIO;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.HOOD_RADIUS;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.MAX_ANGLE;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.MIN_ANGLE;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.MOI;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.STARING_ANGLE;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.AngularControl.*;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.VelocityControl.*;
+import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
 import org.sciborgs1155.robot.shooter.ShooterConstants.VelocityControl;
 
 public class SimShooter implements ShooterIO {
-  private final FlywheelSim flywheel;
+  private final FlywheelSim simFlyWheel;
 
+  /* SingleJoinedArmSim is for a single pivot rotating arm
+   * It also works for the variable hood as their movements are the same 
+   */
+  private final SingleJointedArmSim simHood;
+
+  /* Initalize the fields */
   public SimShooter() {
-    flywheel =
+    simFlyWheel =
         new FlywheelSim(
-            LinearSystemId.identifyVelocitySystem(
-                VelocityControl.V, VelocityControl.A), // physical behavior
+            LinearSystemId.identifyVelocitySystem(VelocityControl.V, VelocityControl.A), // physical behavior
             DCMotor.getKrakenX60(1)); // eletrical behavior
+      
+
+        simHood =  
+          new SingleJointedArmSim(DCMotor.getKrakenX60(1), 
+          GEAR_RATIO, 
+          MOI, 
+          HOOD_RADIUS.in(Meters), 
+          MIN_ANGLE.in(Radians),
+          MAX_ANGLE.in(Radians), 
+          true, 
+          STARING_ANGLE.in(Radians))
   }
 
   @Override
   public void setFlyWheelVoltage(double voltage) {
-    flywheel.setInputVoltage(voltage); // pases the voltage into the flywheel
-    flywheel.update(PERIOD.in(Seconds)); // updates the voltage for 0.02 seconds
+    simFlyWheel.setInputVoltage(voltage); // pases the voltage into the flywheel
+    simFlyWheel.update(PERIOD.in(Seconds)); // updates the voltage for 0.02 seconds
   }
 
   @Override
   public double getFlyWheelVelocity() {
-    return flywheel.getAngularVelocityRadPerSec();
+    return simFlyWheel.getAngularVelocityRadPerSec();
   }
 }
