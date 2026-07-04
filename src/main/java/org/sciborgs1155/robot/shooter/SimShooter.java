41a2eab2f01d7343fde24028dd51c544870ecb10
Tirtha <tirthas970@gmail.com>
2026-07-04 10:53:40 -0400

    finished sim/and did spotless. Hopefully build is successful

--- src/main/java/org/sciborgs1155/robot/shooter/SimShooter.java
+++ src/main/java/org/sciborgs1155/robot/shooter/SimShooter.java
@@ -1,5 +1,6 @@
 package org.sciborgs1155.robot.shooter;
 
+import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.Seconds;
 import static org.sciborgs1155.robot.Constants.PERIOD;
@@ -9,22 +10,18 @@ import static org.sciborgs1155.robot.shooter.ShooterConstants.MAX_ANGLE;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.MIN_ANGLE;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.MOI;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.STARING_ANGLE;
-import static org.sciborgs1155.robot.shooter.ShooterConstants.AngularControl.*;
-import static org.sciborgs1155.robot.shooter.ShooterConstants.VelocityControl.*;
-import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
-
 import org.sciborgs1155.robot.shooter.ShooterConstants.VelocityControl;
 
 public class SimShooter implements ShooterIO {
   private final FlywheelSim simFlyWheel;
 
   /* SingleJoinedArmSim is for a single pivot rotating arm
-   * It also works for the variable hood as their movements are the same 
+   * It also works for the variable hood as their movements are the same
    */
   private final SingleJointedArmSim simHood;
 
@@ -32,19 +29,20 @@ public class SimShooter implements ShooterIO {
   public SimShooter() {
     simFlyWheel =
         new FlywheelSim(
-            LinearSystemId.identifyVelocitySystem(VelocityControl.V, VelocityControl.A), // physical behavior
+            LinearSystemId.identifyVelocitySystem(
+                VelocityControl.V, VelocityControl.A), // physical behavior
             DCMotor.getKrakenX60(1)); // eletrical behavior
-      
 
-        simHood =  
-          new SingleJointedArmSim(DCMotor.getKrakenX60(1), 
-          GEAR_RATIO, 
-          MOI, 
-          HOOD_RADIUS.in(Meters), 
-          MIN_ANGLE.in(Radians),
-          MAX_ANGLE.in(Radians), 
-          true, 
-          STARING_ANGLE.in(Radians))
+    simHood =
+        new SingleJointedArmSim(
+            DCMotor.getKrakenX60(1),
+            GEAR_RATIO,
+            MOI,
+            HOOD_RADIUS.in(Meters),
+            MIN_ANGLE.in(Radians),
+            MAX_ANGLE.in(Radians),
+            true,
+            STARING_ANGLE.in(Radians));
   }
 
   @Override
@@ -54,7 +52,18 @@ public class SimShooter implements ShooterIO {
   }
 
   @Override
+  public void setHoodVoltage(double voltage) {
+    simHood.setInputVoltage(voltage);
+    simHood.update(PERIOD.in(Seconds));
+  }
+
+  @Override
   public double getFlyWheelVelocity() {
     return simFlyWheel.getAngularVelocityRadPerSec();
+  }
+
+  @Override
+  public double getHoodPosition() {
+    return simHood.getAngleRads();
   }
 }
