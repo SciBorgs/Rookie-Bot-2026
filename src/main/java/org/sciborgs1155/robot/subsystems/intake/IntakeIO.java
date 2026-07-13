55947aa100bd6b3e484c9c58283545eeecfe9677
Arthur Zin <zarthy10111@gmail.com>
2026-07-13 09:46:36 -0400

    did some wrok on Slapdown.java

--- src/main/java/org/sciborgs1155/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/org/sciborgs1155/robot/subsystems/intake/IntakeIO.java
@@ -3,6 +3,11 @@ package org.sciborgs1155.robot.subsystems.intake;
 public interface IntakeIO extends AutoCloseable{
 
     /**
+     * Might redo all of intake? Not sure if the system is actually going to need this many files
+     *  Maybe its like the rebuilt intake? not yet sure
+     */
+
+    /**
      * @param power of intake 
      */
     void setPower(double power);
@@ -12,12 +17,6 @@ public interface IntakeIO extends AutoCloseable{
      */
     double current();
 
-    /**
-     * @return 
-     */
-    /**
-    boolean beambreak();
 
     
-    */
 }
--- src/main/java/org/sciborgs1155/robot/subsystems/slapdown/Slapdown.java
+++ src/main/java/org/sciborgs1155/robot/subsystems/slapdown/Slapdown.java
@@ -1,5 +1,173 @@
 package org.sciborgs1155.robot.subsystems.slapdown;
 
-public class Slapdown {
+import org.sciborgs1155.lib.Tuning;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.NotLogged;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.networktables.DoubleEntry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Mechanism;
+
+import static org.sciborgs1155.robot.subsystems.slapdown.SlapdownConstants.*;
+
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.Volts;
+import static org.sciborgs1155.robot.Constants.TUNING;
+
+
+
+public class Slapdown extends SubsystemBase implements AutoCloseable{
+
+    private final SlapdownIO hardware; 
+
+    private final ProfiledPIDController pid = new ProfiledPIDController(P, I, D, CONSTRAINTS);
+
+    private final ArmFeedforward ff = new ArmFeedforward(S, G, V, A);
+      
+    // network tables for live tuning
+    @NotLogged private final DoubleEntry tuningP = Tuning.entry("Robot/tuning/slapdown/tuningP", P);
+    @NotLogged private final DoubleEntry tuningI = Tuning.entry("Robot/tuning/slapdown/tuningI", I);       
+    @NotLogged private final DoubleEntry tuningD = Tuning.entry("Robot/tuning/slapdown/tuningD", D);
+    @NotLogged private final DoubleEntry tuningS = Tuning.entry("Robot/tuning/slapdown/tuningS", S);
+    @NotLogged private final DoubleEntry tuningG = Tuning.entry("Robot/tuning/slapdown/tuningG", G);
+    @NotLogged private final DoubleEntry tuningV = Tuning.entry("Robot/tuning/slapdown/tuningV", V);
+    @NotLogged private final DoubleEntry tuningA = Tuning.entry("Robot/tuning/slapdown/tuningA", A);
+    
+    /** routine for recording + analyzing motor dataq */
+    private final SysIdRoutine sysIdRoutine;
+    /**
+     * @param hardware object will be operated on
+     */
+    public Slapdown(SlapdownIO hardware){
+        this.hardware = hardware;
+
+        pid.setTolerance(POSITION_TOLERANCE.in(Radians));
+        pid.reset(hardware.position());
+        pid.setGoal(START_ANGLE.in(Radians));
+
+        setDefaultCommand(retract());
+
+        sysIdRoutine = new SysIdRoutine(
+            new Config(RAMP_RATE, STEP_VOLTAGE, TIME_OUT),
+            new Mechanism(voltage -> hardware.setVoltage(voltage.in(Volts)), null, this));
+
+        // sets up commands to test / gather data
+            SmartDashboard.putData(
+        "Robot/slapdown/quasistatic forward",
+        sysIdRoutine
+            .quasistatic(Direction.kForward)
+            .until(() -> atPosition(MAX_ANGLE.in(Radians)))
+            .withName("slapdown quasistatic forward"));
+        SmartDashboard.putData(
+            "Robot/slapdown/quasistatic backward",
+            sysIdRoutine
+                .quasistatic(Direction.kReverse)
+                .until(() -> atPosition(MIN_ANGLE.in(Radians)))
+                .withName("slapdown quasistatic backward"));
+        SmartDashboard.putData(
+            "Robot/slapdown/dynamic forward",
+            sysIdRoutine
+                .dynamic(Direction.kForward)
+                .until(() -> atPosition(MAX_ANGLE.in(Radians)))
+                .withName("slapdown dynamic forward"));
+        SmartDashboard.putData(
+            "Robot/slapdown/dynamic backward",
+            sysIdRoutine
+                .dynamic(Direction.kReverse)
+                .until(() -> atPosition(MIN_ANGLE.in(Radians)))
+                .withName("slapdown dynamic backward"));
+    }
+    /**
+     * Command factory
+     * @param angle go to angle
+     * @return command to make intake arm go to angle
+     */
+    public Command goTo(double angle){
+        return run(() -> update(angle)).withName("go to angle");
+    }
+
+    /**
+     * @return slap down intake arm
+     */
+    public Command extend(){
+        return goTo(MIN_ANGLE.in(Radians));
+    }
+    /**
+     * @return bring up intake arm
+     */
+
+    public Command retract(){
+        return goTo(MAX_ANGLE.in(Radians));
+    }
+
+    /**
+     * @return position of slapdown
+     */
+    @Logged
+    public double position(){
+        return hardware.position();
+    }
+
+    /**
+     * @param angle tests if Slapdown is at given angle
+     * @return the test 
+     */
+    public boolean atPosition(double angle){
+        return Math.abs(angle - position()) < POSITION_TOLERANCE.in(Radians);
+    }
+
+    /**
+     * @return the position of the pid
+     */
+    @Logged
+    public double setpoint(){
+        return pid.getSetpoint().position;
+    }
+
+    /**
+     * @return whether or not slapdown is at goal
+     */
+    @Logged
+    public boolean atGoal(){
+        return pid.atGoal();
+    }
+
+    /**
+     * @param angle set the slapdown angle to given angle using PID and feedforward
+     */
+    public void update(double angle){
+        double rads = MathUtil.clamp(angle, MIN_ANGLE.in(Radians), MAX_ANGLE.in(Radians));
+        double pidVoltage = pid.calculate(hardware.position(), rads);
+        double ffVoltage = ff.calculate(pid.getSetpoint().position, pid.getSetpoint().velocity);
+        hardware.setVoltage(pidVoltage + ffVoltage);
+    }
+
+    // turns on live tuning for PID and ff iff TUNING
+    @Override
+    public void periodic(){
+        if(TUNING){
+        pid.setP(tuningP.get());
+        pid.setI(tuningI.get());
+        pid.setD(tuningD.get());
+        ff.setKg(tuningG.get());
+        ff.setKa(tuningA.get());
+        ff.setKv(tuningV.get());
+        ff.setKs(tuningS.get());
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        hardware.close();
+    }
     
 }
