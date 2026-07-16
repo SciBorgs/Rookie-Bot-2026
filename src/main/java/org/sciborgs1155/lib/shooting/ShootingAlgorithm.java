ec92c53034b72827949e88054378241edca40fd4
Tirtha <tirthas970@gmail.com>
2026-07-16 19:19:12 -0400

    did what sunay suggested -> added a interpoalting tree map table

--- src/main/java/org/sciborgs1155/lib/shooting/ShootingAlgorithm.java
+++ src/main/java/org/sciborgs1155/lib/shooting/ShootingAlgorithm.java
@@ -1,5 +1,29 @@
 package org.sciborgs1155.lib.shooting;
 
-public interface ShootingAlgorithm {
-    
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+
+public class ShootingAlgorithm {
+
+    /**
+     * THis is a lookup table
+     * Key is the distance to target
+     * Value is the RPM that is interpolated
+     */
+
+    InterpolatingDoubleTreeMap shooterTable = new InterpolatingDoubleTreeMap();
+
+    public ShootingAlgorithm() {
+
+        //PLACE HOLDER VALUES FOR NOW
+        shooterTable.put(1.0, 1500.0);
+        shooterTable.put(2.0, 2500.0);
+        shooterTable.put(3.0, 3800.0);
+
+    }
+
+    public double getRPM(double distanceToTarget) {
+        return shooterTable.get(distanceToTarget);
+
+    }
+
 }
