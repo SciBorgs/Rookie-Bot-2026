969ae9b55c25e1eceef12eb2c155cf28c54d4123
Tirtha <tirthas970@gmail.com>
2026-07-17 09:24:59 -0400

    started on auto aim method

--- src/main/java/org/sciborgs1155/lib/shooting/ShootingAlgorithm.java
+++ src/main/java/org/sciborgs1155/lib/shooting/ShootingAlgorithm.java
@@ -1,14 +1,18 @@
 package org.sciborgs1155.lib.shooting;
 
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.numbers.N2;
 
 public class ShootingAlgorithm {
 
     /**
-     * THis is a lookup table
+     * Make a interpolating tree map (look up table)
      * Key is the distance to target
      * Value is the RPM that is interpolated
      */
+
 
     InterpolatingDoubleTreeMap shooterTable = new InterpolatingDoubleTreeMap();
 
@@ -21,12 +25,24 @@ public class ShootingAlgorithm {
     }
 
     /**
-     * 
+     * Method to return the desired rpm for the hood
      * @param distanceToTarget The distance to target in some unit (Have to figure it out)
      * @return the interpolated RPM using shooterTable, a tree map 
      */
     public double getRPM(double distanceToTarget) {
         return shooterTable.get(distanceToTarget);
+    }
+
+    /**
+     * Method to find the velocity as a vector (direction and spped) to run the shooter to shoot accurately
+     * 
+     * @param shooterPosition Positon of shooter as a transaltion3D
+     * @param targetPosition Postion of the target as a translation3D
+     * @param shooterVelocity current flywheel velocity
+     */
+    public void autoAim(Translation3d shooterPosition, Translation3d targetPosition, Vector<N2> shooterVelocity) {
+
+        Translation3d displacementVector = targetPosition.minus(shooterPosition);
 
     }
 
--- src/main/java/org/sciborgs1155/robot/FieldConstants.java
+++ src/main/java/org/sciborgs1155/robot/FieldConstants.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Distance;
@@ -111,4 +112,5 @@ public class FieldConstants {
   }
 
   // List field constants below!
+
 }
