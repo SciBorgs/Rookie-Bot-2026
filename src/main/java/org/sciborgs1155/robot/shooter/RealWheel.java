6cbab83e9b4684fb84502140504ce9b9fe21a63e
Tirtha <tirthas970@gmail.com>
2026-07-22 11:05:20 -0400

    detled useless imports now that i swtiched to sparkflex

--- src/main/java/org/sciborgs1155/robot/shooter/RealWheel.java
+++ src/main/java/org/sciborgs1155/robot/shooter/RealWheel.java
@@ -4,9 +4,6 @@ import static edu.wpi.first.units.Units.Amps;
 import static org.sciborgs1155.robot.Ports.Shooter.*;
 import static org.sciborgs1155.robot.shooter.ShooterConstants.*;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkFlex;
@@ -15,11 +12,6 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.ResetMode;
 
-
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkFlex;
-
-import org.sciborgs1155.lib.FaultLogger;
-import org.sciborgs1155.lib.TalonUtils;
 
 public class RealWheel implements WheelIO {
 
