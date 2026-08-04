3c88032508005a521f6fe0a5e2fd7ca6557e45b6
Tirtha <tirthas970@gmail.com>
2026-08-04 09:43:44 -0400

    set up shooting.java, but I think i need drive to be done first

--- src/main/java/org/sciborgs1155/robot/commands/Shooting.java
+++ src/main/java/org/sciborgs1155/robot/commands/Shooting.java
@@ -1,9 +1,29 @@
 package org.sciborgs1155.robot.commands;
 import org.sciborgs1155.robot.shooter.Shooter;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+import org.sciborgs1155.robot.commands.shooting.FuelVisualizer;
 import org.sciborgs1155.robot.hood.Hood;
+import org.sciborgs1155.robot.commands.shooting.MovingShot;
 public class Shooting {
 
-    Shooter Shooter;
-    
+    private final MovingShot movingShot = new MovingShot();
+
+    private final Shooter shooter;
+    private final Hood hood;
+    private final FuelVisualizer fuelVisualizer;
+    //TODO: ADD INTAKE LATER
+
+    public Shooting(Shooter shooter, Hood hood, FuelVisualizer fuelVisualizer) {
+        this.shooter = shooter;
+        this.hood = hood;
+        this.fuelVisualizer = fuelVisualizer;
+    }
+
+    //void for now 
+    public void calculateShot(Translation2d target) {
+
+    }
     
 }
