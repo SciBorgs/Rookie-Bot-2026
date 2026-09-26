a1efe305ebdd4b88cf81956d046aa4da4bf67d04
Oscar Liu <oscarpro6908@gmail.com>
2026-09-25 20:46:50 -0400

    Reverted drive changes

--- src/main/java/org/sciborgs1155/robot/drive/Drive.java
+++ src/main/java/org/sciborgs1155/robot/drive/Drive.java
@@ -62,9 +62,9 @@ public class Drive extends SubsystemBase {
   private final PIDController leftPidController =
       new PIDController(DrivePID.kP, DrivePID.kI, DrivePID.kD);
   private final PIDController rightPIDController =
-      new PIDController(DrivePID.kP, DrivePID.kI, DrivePID.kD);
+      new PIDController(DrivePID.kP, DrivePID.kI, DrivePID.kP);
   private final PIDController headingPID =
-      new PIDController(HeadingPID.kP, HeadingPID.kI, HeadingPID.kD);
+      new PIDController(HeadingPID.kP, HeadingPID.kP, HeadingPID.kP);
 
   private final DifferentialDrivetrainSim driveSim;
 
@@ -141,9 +141,9 @@ public class Drive extends SubsystemBase {
     final double leftFeedforward = feedforward.calculate(realLeftSpeed);
     final double rightFeedforward = feedforward.calculate(realRightSpeed);
 
-    final double leftPID = leftPidController.calculate(leftVelocity(), realLeftSpeed);
+    final double leftPID = leftPidController.calculate(leftEncoder.getVelocity(), realLeftSpeed);
     final double rightPID =
-        rightPIDController.calculate(rightVelocity(), realRightSpeed);
+        rightPIDController.calculate(rightEncoder.getVelocity(), realRightSpeed);
 
     double leftVoltage = leftPID + leftFeedforward;
     double rightVoltage = rightPID + rightFeedforward;
@@ -211,18 +211,10 @@ public class Drive extends SubsystemBase {
    * @return the robot chasssis speeds
    */
   public ChassisSpeeds robotRelativeChassisSpeeds() {
-    double leftMotorVelocity = leftVelocity();
-    double rightMotorVelocity = rightVelocity();
+    double leftMotorVelocity = leftEncoder.getVelocity();
+    double rightMotorVelocity = rightEncoder.getVelocity();
     return kinematics.toChassisSpeeds(
         new DifferentialDriveWheelSpeeds(leftMotorVelocity, rightMotorVelocity));
-  }
-
-  private double leftVelocity() {
-    return Robot.isReal() ? leftEncoder.getVelocity() : driveSim.getLeftVelocityMetersPerSecond();
-  }
-
-  private double rightVelocity() {
-    return Robot.isReal() ? rightEncoder.getVelocity() : driveSim.getRightVelocityMetersPerSecond();
   }
 
   /**
